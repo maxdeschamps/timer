55c2a9ecfecdefdfd98a2866ae32159189031315
Jean-Philippe BOURDAIS <45393950+Jpbourdais@users.noreply.github.com>
2022-06-03 10:06:17 +0200

    Feature/add nav bar (#1)
    * add navbar
    
    * add navbar
    
    * add sandouich lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {RouterModule, Routes} from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { TaskComponent } from './components/task/task.component';
+import { SandouichModule } from 'sandouich';
 
 const routes: Routes = [
   {path: 'home', component: AppComponent},
@@ -21,6 +22,7 @@ const routes: Routes = [
     AppRoutingModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
+    SandouichModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
