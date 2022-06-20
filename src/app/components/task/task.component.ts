0ad76afd9ac20e2aed3310b14a580b961275ff50
Maxence Deschamps <maxence.deschamps.pro@gmail.com>
2022-06-20 14:30:57 +0200

    un manager peut modifier les tâches d'un autre collaborateur

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -73,13 +73,13 @@ export class TaskComponent implements OnInit {
   }
 
   projectSelectedChanged(projectId: number) {
-    this.selectedProject.setValue(projectId)
-    this.refreshUserTasks()
+    this.selectedProject.setValue(projectId);
+    this.refreshUserTasks();
   }
 
   userSelectedChanged(userId: any) {
-    this.selectedUser.setValue(userId)
-    this.refreshUserTasks()
+    this.selectedUser.setValue(userId);
+    this.refreshUserTasks();
   }
 
   submitUserTask() {
@@ -89,8 +89,8 @@ export class TaskComponent implements OnInit {
         id: this.id,
         title: this.title.value,
         start: this.start_date.value,
-        end:  this.end_date.value,
-        user_id: user.id,
+        end: this.end_date.value,
+        user_id: this.userService.loggedUserIsAdmin() ? this.selectedUser.value : user.id,
         project_id: this.selectedProject.value,
       }
 
@@ -102,7 +102,7 @@ export class TaskComponent implements OnInit {
   }
 
   deleteTask() {
-    if (window.confirm(`Êtes-vous sûr de vouloir supprimer la tâche '${this.title.value}'`)) {
+    if (window.confirm(`Êtes-vous sûr de vouloir supprimer la tâche '${this.title.value}' ?`)) {
       this.taskService.deleteTask(this.id).subscribe(item => {
         this.modalService.disable();
         this.refreshUserTasks()
@@ -123,24 +123,24 @@ export class TaskComponent implements OnInit {
   };
 
   handleEventClick(eventClickInfo: any) {
-    this.openModal()
-    this.id = eventClickInfo.event._def.publicId
-    this.title.setValue(eventClickInfo.event._def.title)
-    this.start_date.setValue(eventClickInfo.event._instance.range.start.toISOString().substring(0, 16))
-    this.end_date.setValue(eventClickInfo.event._instance.range.end.toISOString().substring(0, 16))
+    this.openModal();
+    this.id = eventClickInfo.event._def.publicId;
+    this.title.setValue(eventClickInfo.event._def.title);
+    this.start_date.setValue(eventClickInfo.event._instance.range.start.toISOString().substring(0, 16));
+    this.end_date.setValue(eventClickInfo.event._instance.range.end.toISOString().substring(0, 16));
   }
 
   handleDateClick(arg: any) {
     this.openModal()
-    this.start_date.setValue(`${arg.dateStr}T08:01`)
-    this.end_date.setValue(`${arg.dateStr}T13:01`)
+    this.start_date.setValue(`${arg.dateStr}T08:00`);
+    this.end_date.setValue(`${arg.dateStr}T12:00`);
   }
 
   openModal() {
-    this.id = null
-    this.title.setValue("")
-    this.start_date.setValue("")
-    this.end_date.setValue("")
+    this.id = null;
+    this.title.setValue("");
+    this.start_date.setValue("");
+    this.end_date.setValue("");
     this.modalService.enable();
   }
 
