603631a1b1b7e8e62a18495d190142165cc2f7bc
Jean-Philippe Bourdais <jeanphi.bourdais@gmail.com>
2022-06-21 00:45:12 +0200

    update models

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -2,8 +2,8 @@ export interface Task {
   id: number|null;
   title?: string;
   description?: string;
-  start?: Date;
-  end?: Date;
-  user_id?: number;
-  project_id?: number;
+  start: Date;
+  end: Date;
+  user_id: number;
+  project_id: number;
 }
